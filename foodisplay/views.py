02250312132aeeb6ff9404a2473dcec3cc622544
yishiyu <814384981@qq.com>
2021-02-27 15:32:55 +0800

    修改了模板文件,能看了...

--- foodisplay/views.py
+++ foodisplay/views.py
@@ -4,5 +4,8 @@ from foodisplay.models import Food
 
 @app.route('/')
 def index():
-    food_list = Food.query.limit(10)
+    food_list = Food.query.limit(30)
+    for food in food_list:
+        food.Ingredients = food.Ingredients.replace("|||||",'、')
+        food.Ingredients = food.Ingredients.replace('|',' ')
     return render_template("index.html",food_list=food_list)
